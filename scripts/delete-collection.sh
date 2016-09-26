79fb0bd335cb544fdab24a391bd97c9653c938d6
gabescarbrough <gabescarbrough@gmail.com>
2016-09-26 16:08:16 -0400

    Write scripts to test CRUD actions
    Wrote cURL requests to test create, get collection, get collections, update and delete

---
+++ scripts/delete-collection.sh
@@ -0,0 +1,2 @@
+curl --include --request DELETE "http://localhost:3000/collections/3" \
+--header "Authorization: Token token=BAhJIiU5NTIwYTVkNGUzZDc0MjNjNGI4ODU2ZGRjOGFlZjVkNAY6BkVG--92e3b7df85a7f55d9d7944750acf58ccf35fc930" \
