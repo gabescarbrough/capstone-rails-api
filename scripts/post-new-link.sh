f918859d5e7f4bf305332045dad11a8178ad3c26
gabescarbrough <gabescarbrough@gmail.com>
2016-09-27 16:21:31 -0400

    Edit post-new-link test script
    Correct formatting and include collection_id

--- scripts/post-new-link.sh
+++ scripts/post-new-link.sh
@@ -1,9 +1,11 @@
 curl --include --request POST "http://localhost:3000/links" \
-  --header "Authorization: Token token=BAhJIiU5NTIwYTVkNGUzZDc0MjNjNGI4ODU2ZGRjOGFlZjVkNAY6BkVG--92e3b7df85a7f55d9d7944750acf58ccf35fc930" \
+  --header "Authorization: Token token=BAhJIiU5Y2VlMzRhNzhmYzUyM2RkMDVmNWI0MmZmYmJkNDgzZQY6BkVG--6afef6eac82c3b911712c617c3d2ca268729299d" \
   --header "Content-Type: application/json" \
   --data '{
     "link": {
-      "name": "Another Test Collection",
-      "description": "This is another test collection"
+      "name": "Test Link",
+      "url": "www.google.com",
+      "description": "This is a test link",
+      "collection_id": "1"
     }
   }'
