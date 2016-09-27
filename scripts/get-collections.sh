c9ab1dc807f48f26cd2261d5bf409c587a952147
gabescarbrough <gabescarbrough@gmail.com>
2016-09-27 14:29:20 -0400

    Write cURL test scripts
    Check that creating link, and getting collections, work

--- scripts/get-collections.sh
+++ scripts/get-collections.sh
@@ -1,1 +1,11 @@
+# GET all collections
 curl --include --request GET http://localhost:3000/collections
+
+
+# GET current_user collections
+curl --include --request GET "http://localhost:3000/collections?current_user=true" \
+--header "Authorization: Token token=BAhJIiVjYzRlZmM2YTE3YzA3NmY2NDNmZGE4M2QxM2YzZDIwNgY6BkVG--f3ae667a3488de72a843ac48d3aac9dbbb154be3" \
+
+
+# GET specific user collections
+curl --include --request GET http://localhost:3000/collections?user_id=3
--- scripts/post-new-link.sh
+++ scripts/post-new-link.sh
@@ -1,8 +1,8 @@
-curl --include --request POST "http://localhost:3000/collections" \
+curl --include --request POST "http://localhost:3000/links" \
   --header "Authorization: Token token=BAhJIiU5NTIwYTVkNGUzZDc0MjNjNGI4ODU2ZGRjOGFlZjVkNAY6BkVG--92e3b7df85a7f55d9d7944750acf58ccf35fc930" \
   --header "Content-Type: application/json" \
   --data '{
-    "collection": {
+    "link": {
       "name": "Another Test Collection",
       "description": "This is another test collection"
     }
