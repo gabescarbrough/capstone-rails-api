e585ad3d2dc3a9933b56dd653bf634503af1aadc
gabescarbrough <gabescarbrough@gmail.com>
2016-09-27 10:09:39 -0400

    Add user reference to links
    Generate migration to create one user has many links relationship

--- app/serializers/link_serializer.rb
+++ app/serializers/link_serializer.rb
@@ -1,6 +1,8 @@
 class LinkSerializer < ActiveModel::Serializer
   attributes :id, :name, :url, :description, :collection
 
+  has_one :user
+
   def collection
     object.collection_id
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926224522) do
+ActiveRecord::Schema.define(version: 20160927140325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,9 +42,11 @@ ActiveRecord::Schema.define(version: 20160926224522) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "collection_id"
+    t.integer  "user_id"
   end
 
   add_index "links", ["collection_id"], name: "index_links_on_collection_id", using: :btree
+  add_index "links", ["user_id"], name: "index_links_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
@@ -60,4 +62,5 @@ ActiveRecord::Schema.define(version: 20160926224522) do
   add_foreign_key "collections", "users"
   add_foreign_key "examples", "users"
   add_foreign_key "links", "collections"
+  add_foreign_key "links", "users"
 end
