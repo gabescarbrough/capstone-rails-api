d67471ae591de66bacfafe447d4e96f6fc2ad5e2
gabescarbrough <gabescarbrough@gmail.com>
2016-10-15 21:41:15 -0400

    Add link_check to link model
    Link model now checks for 'http://' and adds if not present.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,17 @@
+require 'uri'
+
 class Link < ActiveRecord::Base
   belongs_to :collection
   belongs_to :user
+
+  before_save :url_check
+
+  protected
+
+  def url_check
+    u = URI(url)
+    if (!u.scheme)
+        self.url = "http://#{url}"
+    end
+  end
 end
